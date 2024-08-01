956023273a259ae2b52e53319ce14199dcc0e440
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-01 20:00:45 +0545

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,8 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import Video from './video.jsx';
 
-import Button from "./Button"
 function App() {
-  return(
-    <Button/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Video Player Example</h1>
+        <Video />
+      </header>
+    </div>
   );
 }
-export default App
+
+export default App;
