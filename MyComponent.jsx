7c8eef01bec6fef0da3e77ef17845dc7550c7c8c
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-05 13:31:55 +0545

    Add files via upload

--- MyComponent.jsx
+++ MyComponent.jsx
@@ -1,27 +1,30 @@
-import { useState } from "react";
 
+import React,{useState} from "react";
 
 function MyComponent(){
-    const [count, setCount] = useState(0);
+    const[car, setCar] = useState({years:2024, make:"Ford", model:"Mustang"});
 
-    function increment(){
-        setCount(count + 1);
+    function handleYearChange(event){
+        setCar({...car,years:event.target.value});
         
-    };
-    function decrement(){
-        setCount(count -1);
+
     }
-    function reset(){
-        setCount(count =0);
+    function handmakeChange(event){
+        setCar ({...car,make:event.target.value});
+
     }
-    return(
-        <div>
-        <p>Count:{count}</p>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={increment}>Increment</button>
-        <button onClick={reset}>Reset</button>
-        
-        </div>
-    );
+    function handlemodelChange(event){
+        setCar({...car,model:event.target.value});
+         
+    }
+
+    return(<div>
+        <p>Your favourate car is: {car.years} {car.make} {car.model}</p>
+
+        <input type="number" value={car.years} onChange={handleYearChange}/><br/>
+        <input type="text" value={car.make}  onChange={handmakeChange}/><br />
+        <input type="text" value={car.model}onChange={handlemodelChange} /><br />
+    </div>)
+    
 }
 export default MyComponent
